ade10cdf157d87f6e14f605023d991bcd2b84e37
Browqz <92259633+browqz@users.noreply.github.com>
2023-06-27 05:56:03 +0200

    Add files via upload

---
+++ purge.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+TOKEN = 'TON_TOKEN'
+PREFIX = '+'  
+
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Salut je suis :  {bot.user.name}')
+
+@bot.command()
+async def nigga(ctx, nombre: int):
+    # Supprimer les messages dans le canal actuel
+    await ctx.channel.purge(limit=nombre + 1)
+    print(f'{nombre}  messages ont bien été supprimes dans le salon : {ctx.channel.name}')
+
+bot.run(TOKEN)
