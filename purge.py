9fcb9187a307252ffed5d1cee283ef1e561aaaa0
Browqz <92259633+browqz@users.noreply.github.com>
2023-10-26 00:06:53 +0200

    purge.py

--- purge.py
+++ purge.py
@@ -4,19 +4,21 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-TOKEN = 'TON_TOKEN'
+TOKEN = 'MON_TOKEN'
 PREFIX = '+'  
 
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Salut je suis :  {bot.user.name}')
+    print(f'Salut, je suis :  {bot.user.name}')
 
 @bot.command()
-async def nigga(ctx, nombre: int):
-
-    await ctx.channel.purge(limit=nombre + 1)
-    print(f'{nombre}  messages ont bien été supprimes dans le salon : {ctx.channel.name}')
+async def clear(ctx, nombre: int):
+    if nombre > 0:
+        await ctx.channel.purge(limit=nombre + 1)
+        print(f'{nombre} messages ont été supprimés dans le salon : {ctx.channel.name}')
+    else:
+        await ctx.send("Le nombre doit être supérieur à zéro.")
 
 bot.run(TOKEN)
